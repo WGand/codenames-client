9e337b628ba718d26d3b069675fefe5a48eefdeb
WGand <wagandino.18@est.ucab.edu.ve>
2023-07-15 00:18:16 -0400

    u

--- codenames-frontend/src/app/app.component.ts
+++ codenames-frontend/src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { GameService } from './game.service';
+import { Board } from './board.interface';
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>Codenames Game</h1>
-    <app-game-board></app-game-board>
+    <app-main-menu (teamSelected)="handleTeamSelected($event.team)"></app-main-menu>
+    <ng-container *ngIf="selectedTeam !== '' && board">
+      <app-game-board [team]="selectedTeam" [board]="board"></app-game-board>
+    </ng-container>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  selectedTeam: string = '';
+  board: Board | undefined;
+
+  constructor(private gameService: GameService) { }
+
+  handleTeamSelected(team: string): void {
+    this.selectedTeam = team;
+    if (team === 'RED' || team === 'BLUE') {
+      this.gameService.getBoards().subscribe(boards => {
+        this.board = boards[0];
+      });
+    }
+  }
 }
